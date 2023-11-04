13d6cd1f6f4e89389384ae552b3138a640be4cd8
fibbanachi <49879947+Programmeris@users.noreply.github.com>
2023-11-04 13:15:00 +0300

    added get commands subparsers

--- pgh.py
+++ pgh.py
@@ -3,10 +3,15 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
     parser.description = "Simple utility for interactive with PostgreSQL"
-    parser.add_argument("-h", "--host", required=True)
+    parser.add_argument("-a", "--host", required=True)
     parser.add_argument("-u", "--username", required=True)
     parser.add_argument("-p", "--password", required=True)
     parser.add_argument("-q", "--raw-sql")
+
+    subcommandParser = parser.add_subparsers(title="subcommands")
+    getTablesCommandParser = subcommandParser.add_parser("get tables")
+    getSequencesCommandParser = subcommandParser.add_parser("get sequences")
+
     args = parser.parse_args()
 
 if __name__ == '__main__':
