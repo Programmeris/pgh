5f4a826d12f322b7ff2b424be780db1eb00c0a2a
fibbanachi <49879947+Programmeris@users.noreply.github.com>
2023-11-05 19:20:46 +0300

    implement get tables command handler

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
+	"database/sql"
+	_ "github.com/lib/pq"
+	"fmt"
+)
 
 func main() {
 	initCommandFlags()
@@ -8,15 +13,23 @@ func main() {
 }
 
 func initCommandFlags()  {
-	pghCmd.PersistentFlags().StringP("hostname", "a", "localhost", "")
-	pghCmd.PersistentFlags().StringP("port", "p", "5432", "")
-	pghCmd.PersistentFlags().StringP("username", "U", "", "")
-	pghCmd.PersistentFlags().StringP("password", "P", "", "")
-	pghCmd.PersistentFlags().StringP("database", "d", "", "")
+	pghCmd.PersistentFlags().StringVar(&hostname, "a", "localhost", "")
+	pghCmd.PersistentFlags().StringVar(&port, "p", "5432", "")
+	pghCmd.PersistentFlags().StringVar(&username, "U", "", "")
+	pghCmd.PersistentFlags().StringVar(&password, "P", "", "")
+	pghCmd.PersistentFlags().StringVar(&database, "d", "", "")
 
 	getCmd.AddCommand(tablesCmd)
 	pghCmd.AddCommand(getCmd)
 }
+
+var ( 
+	hostname string
+	port string
+	username string
+	password string
+	database string
+)
 
 var pghCmd = &cobra.Command{
 	Use:   "pgh",
@@ -30,4 +43,29 @@ var getCmd = &cobra.Command{
 
 var tablesCmd = &cobra.Command{
 	Use:   "tables",
+	Run: func(cmd *cobra.Command, args []string) {
+		var connString = "user=" + username + " password=" + password + " dbname=" + database + " sslmode=disable"
+		db, err := sql.Open("postgres", connString)
+
+		defer db.Close()
+
+		if err != nil {
+			panic(err)
+		}
+
+		tables, err := db.Query("SELECT * FROM pg_catalog.pg_tables")
+
+		if err != nil {
+			panic(err)
+		}
+
+		for tables.Next() {
+			var name string
+			if err := tables.Scan(&name); err != nil {
+				panic(err)
+			}
+			fmt.Printf("this is something: %s\n", name)
+		}
+
+	},
 }
