1cae8cfb0e558620e18f15bf30038d149f09fdb0
fibbanachi <49879947+Programmeris@users.noreply.github.com>
2023-11-19 22:45:48 +0000

    add ConnectionOptions structure

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -6,6 +6,18 @@ import (
 	"fmt"
 )
 
+type ConnectionOptions struct{
+	hostname string
+	port string
+	username string
+	password string
+	database string
+}
+
+func NewConnectionOptions()  {
+	
+}
+
 func GetRowsFromTable(hostname string, port string, username string, password string, database string, columns string, table string) *sql.Rows  {
 	var connString = "host=" + hostname + " port=" + port + " user=" + username + " password=" + password + " dbname=" + database + " sslmode=disable"
 	db, err := sql.Open("postgres", connString)
