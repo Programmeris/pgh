4cc182dc4ad5dbe22c205b75dcfede3ec925bb17
fibbanachi <49879947+Programmeris@users.noreply.github.com>
2023-11-19 17:40:03 +0000

    implement getRowsFromDb func

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,5 +1,25 @@
 package util
 
-func getRowsFromDb()  {
-	
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+)
+
+func getRowsFromDb(hostname string, port string, username string, password string, database string) *sql.Rows  {
+	var connString = "host=" + hostname + " port=" + port + " user=" + username + " password=" + password + " dbname=" + database + " sslmode=disable"
+	db, err := sql.Open("postgres", connString)
+
+	defer db.Close()
+
+	if err != nil {
+		panic(err)
+	}
+
+	rows, err := db.Query("SELECT tablename FROM pg_catalog.pg_tables")
+
+	if err != nil {
+		panic(err)
+	}
+
+	return rows
 }
