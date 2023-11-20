bc98f7b75619756c4b98c323adf6e2451b5e57b9
fibbanachi <49879947+Programmeris@users.noreply.github.com>
2023-11-21 00:05:59 +0300

    refactoring locksCmd

--- pkg/cmd/get.go
+++ pkg/cmd/get.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"database/sql"
 	"pgh/pkg/util"
 	_ "github.com/lib/pq"
 	"github.com/spf13/cobra"
@@ -69,20 +68,8 @@ var matViewsCmd = &cobra.Command{
 var locksCmd = &cobra.Command{
 	Use:   "locks",
 	Run: func(cmd *cobra.Command, args []string) {
-		var connString = "host=" + hostname + " port=" + port + " user=" + username + " password=" + password + " dbname=" + database + " sslmode=disable"
-		db, err := sql.Open("postgres", connString)
-
-		defer db.Close()
-
-		if err != nil {
-			panic(err)
-		}
-
-		locks, err := db.Query("SELECT locktype FROM pg_catalog.pg_locks")
-
-		if err != nil {
-			panic(err)
-		}
+		var connectionOptions = util.NewConnectionOptions(hostname, port, username, password, database)
+		var locks = util.GetRowsFromTable(*connectionOptions, "locktype", "pg_catalog.pg_locks")
 
 		util.PrintRows(locks, "Locks")
 
