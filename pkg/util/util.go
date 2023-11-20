3df549112a0c625b4fd7ba39f119af314df08022
fibbanachi <49879947+Programmeris@users.noreply.github.com>
2023-11-20 08:02:14 +0000

    added buildConnectionString func

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -26,6 +26,13 @@ func NewConnectionOptions(hostname string, port string, username string, passwor
 
 }
 
+func buildConnectionString(connectionOptions *ConnectionOptions) string {
+	return "host=" + connectionOptions.hostname + " port=" + 
+			connectionOptions.port + " user=" + connectionOptions.username + 
+			" password=" + connectionOptions.password + " dbname=" + 
+			connectionOptions.database + " sslmode=disable"
+}
+
 func GetRowsFromTable(hostname string, port string, username string, password string, database string, columns string, table string) *sql.Rows  {
 	var connString = "host=" + hostname + " port=" + port + " user=" + username + " password=" + password + " dbname=" + database + " sslmode=disable"
 	db, err := sql.Open("postgres", connString)
